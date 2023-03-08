e5fe0fed1d1222d075e4062a8f7fe6cee2ccbed5
Christopher McGurn <cpmcgurn@proton.me>
2023-03-08 19:10:57 +0000

    Setup server

---
+++ app.js
@@ -0,0 +1,28 @@
+/**
+ * Requires
+ */
+const express = require('express');
+const ejs = require('ejs');
+
+/**
+ * Configuration
+ */
+const app = express();
+const port = 3000;
+const host = 'localhost';
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+/**
+ * Route handling
+ */
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
+
+/**
+ * Start server
+ */
+app.listen(port, host, () => {
+  console.log(`Server started on port ${port}`);
+})

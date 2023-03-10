efcf53987b32c313eb89fc46472f9395262279de
Christopher McGurn <cpmcgurn@proton.me>
2023-03-10 15:37:43 +0000

    Handle post routing, allowing for getting and storing user input

--- app.js
+++ app.js
@@ -8,22 +8,33 @@ const ejs = require('ejs');
  * Configuration
  */
 const app = express();
-const port = 3000;
-const host = 'localhost';
+const PORT = 3000;
+const HOST = 'localhost';
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 /**
+ * App variables
+ */
+ const todoItems = [];
+
+/**
  * Route handling
  */
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.render('todo');
 })
+
+app.post('/', (req, res) => {
+  const newTodoItem = req.body.todo;
+  todoItems.push(newTodoItem);
+  res.redirect('/');
+});
 
 /**
  * Start server
  */
-app.listen(port, host, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server started on port ${PORT}`);
 })
